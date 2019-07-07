0e13b6fe2c077d92b0770040547268886377f907
Rob Earlam <robert.earlam@gmail.com>
2019-07-07 11:35:52 +1000

    Movie detail page added

--- src/DddMelb2019.Web/Pages/CastMember.cshtml.cs
+++ src/DddMelb2019.Web/Pages/CastMember.cshtml.cs
@@ -1,26 +1,21 @@
-﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using DddMelb2019.Web.Context;
-using DddMelb2019.Web.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
 
 namespace DddMelb2019.Web.Pages
 {
-    public class IndexModel : PageModel
+    public class CastMemberModel : PageModel
     {
         private readonly MovieSiteContext movieSiteContext;
 
-        public List<Movie> Movies { get; set; }
-
-        public IndexModel(MovieSiteContext movieSiteContext)
+        public CastMemberModel(MovieSiteContext movieSiteContext)
         {
             this.movieSiteContext = movieSiteContext;
         }
 
         public async Task OnGetAsync()
         {
-            Movies = await movieSiteContext.Movies.ToListAsync();
+            
         }
     }
 }
---
+++ src/DddMelb2019.Web/Pages/Movie.cshtml.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DddMelb2019.Web.Context;
+using DddMelb2019.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DddMelb2019.Web.Pages
+{
+    public class MovieModel : PageModel
+    {
+        private readonly MovieSiteContext movieSiteContext;
+        public Movie Movie { get; set; }
+        public List<CastMember> Cast { get; set; }
+
+        public MovieModel(MovieSiteContext movieSiteContext)
+        {
+            this.movieSiteContext = movieSiteContext;
+        }
+
+        public IActionResult OnGet(int movieId)
+        {
+            Movie = movieSiteContext.Movies.Where(x => x.MovieId == movieId).FirstOrDefault();
+            if(Movie == null)
+                return Redirect("/");
+
+            Cast = movieSiteContext.MovieCastMembers.Where(x => x.MovieId == movieId).Select(x => x.CastMember).ToList();
+
+            return Page();
+        }
+    }
+}
